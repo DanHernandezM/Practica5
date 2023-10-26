cdd98b7dd9188988a808ce3ab8141b183abd292a
DanHernandezM <148841520+DanHernandezM@users.noreply.github.com>
2023-10-25 21:00:46 -0600

    Fin del primer tema

--- src/main/java/com/mycompany/practica5/Ejecutor.java
+++ src/main/java/com/mycompany/practica5/Ejecutor.java
@@ -13,13 +13,37 @@ import java.io.IOException;
 
 public class Ejecutor extends TareaPeriodica{
 	
-	private String comando;
+	protected String comando;
 	
 	public Ejecutor(String comando, int periodo){
 		super(periodo);
 		this.comando = comando;
 	}
 	
-	public String leerComando(){ return comando; }
-		
-}
+	public String leerComando(){
+            return comando;
+        }
+	
+        public boolean necesitaEjecucion() {
+            if (!activa) return false;
+            
+        Calendar proximaEjecucion = new GregorianCalendar();
+            proximaEjecucion.setTime(ultimaEjecucion);
+            proximaEjecucion.add(Calendar.SECOND, periodo);
+            
+            Calendar ahora = new GregorianCalendar(); 
+            
+             return (proximaEjecucion.before(ahora)); 
+        }
+        public int ejecutarTarea() {    
+            try {        
+                Runtime.getRuntime().exec(comando);
+                return 0;    
+            } 
+            catch(IOException e) {       
+                System.err.println(e.toString());    
+            }    
+            return -1; 
+        } 
+
+        }
